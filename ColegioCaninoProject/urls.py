d9b8a0694ac56e289884e8c5081b7cb3fbddb024
Dumbo4nonimo <144817276+Dumbo4nonimo@users.noreply.github.com>
2025-11-04 06:05:40 -0500

    guardar

--- ColegioCaninoProject/urls.py
+++ ColegioCaninoProject/urls.py
@@ -22,4 +22,3 @@ urlpatterns = [
     path('api/', include('login.api.urls')),
 ]
 
-
--- login/api/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from login.api.views import clienteProfileViewSet
-
-router = DefaultRouter()
-router.register('cliente', clienteProfileViewSet, basename='cliente')
-
-urlpatterns = router.urls
